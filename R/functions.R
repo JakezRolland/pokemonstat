fa709c86b5d4c59d8e3166315eb7b2e8e8e60497
Jakez <jakez.rolland@7opteam.com>
2019-03-19 19:23:23 +0100

    fix shinyodd when n is nan or na

--- R/functions.R
+++ R/functions.R
@@ -336,6 +336,9 @@ shinyOdd<-function(nPotentialShinyCatched){
   functionName<-match.call()[[1]]
   step<-"Start"
   tryCatch({
+    if(is.nan(nPotentialShinyCatched) || is.na(nPotentialShinyCatched)){
+      return(0)
+    }
   return(round(sum(dbinom(x=1:nPotentialShinyCatched,size=nPotentialShinyCatched,prob=1/450))*100,2))
   }, error = function(err) onError(err,functionName,step ))
 }
